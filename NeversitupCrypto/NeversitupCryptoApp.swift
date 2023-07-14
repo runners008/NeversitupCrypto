3e1e0ca603461fc4ee6b66aab96f243a6c52a2b5
runnsss <runnsiriphuwanich@gmail.com>
2023-07-14 14:25:43 +0700

    Initial Commit

---
+++ NeversitupCrypto/NeversitupCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeversitupCryptoApp.swift
+//  NeversitupCrypto
+//
+//  Created by Runn Siriphuwanich on 14/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct NeversitupCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
